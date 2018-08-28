b7c517b66c3af8c03f1a39fe2efb7718d0c60c59
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-08-28 17:42:46 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <h2>I am Vaibhav Yadav</h2>
+    <p>Learning to code full time independently</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
