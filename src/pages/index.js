215e1d4c1729810d92afc8b70ac6000bba6abde7
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-08-28 20:53:01 +0530

    basic styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,19 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
+  <div
+    style={{
+      textAlign: 'center',
+      background: '#eee',
+      padding: '15px',
+      border: '5px solid rgb(102, 51, 153)',
+      borderRadius: '20px',
+      fontFamily: 'Arial, Helvetica, sans-serif',
+    }}
+  >
+    <h1>Hi !</h1>
     <h2>I am Vaibhav Yadav</h2>
-    <p>Learning to code full time independently</p>
+    <p>Right now i am learning to code full time independently</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
